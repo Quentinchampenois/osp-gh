a54ede9f8b71a42c249878c5480b2808a444fd7d
quentinchampenois <26109239+Quentinchampenois@users.noreply.github.com>
2023-12-08 19:23:01 +0100

    feat: allow to resume inline

--- main.go
+++ main.go
@@ -18,20 +18,42 @@ func (r *Repository) addPR(pr PR) {
 	r.PRs = append(r.PRs, pr)
 }
 
+func (r *Repository) resumeInLines() {
+	fmt.Printf("%s (PRs: %d) \n", r.Name, len(r.PRs))
+	for _, pr := range r.PRs {
+		parsedDate, _ := time.Parse(time.RFC3339, pr.Date)
+		creationDateAgo := timeAgo(parsedDate)
+		fmt.Printf("* #%s %s - %s %s \n", pr.ID, pr.Title, pr.State, creationDateAgo)
+	}
+}
+
 func (r *Repository) resume() {
 	fmt.Printf("%s \n   Number of PRs : %d\n", r.Name, len(r.PRs))
 	for _, pr := range r.PRs {
+		parsedDate, _ := time.Parse(time.RFC3339, pr.Date)
+		creationDateAgo := timeAgo(parsedDate)
 		if pr.isOpened() {
-			dt, _ := time.Parse(time.DateTime, pr.Date)
-
-			fmt.Printf("   #%s ready for review - %s \n", pr.ID, dt.Format("01-02-2006"))
+			fmt.Printf("   #%s Ready - %s \n", pr.ID, creationDateAgo)
 			fmt.Printf("       https://github.com/%s/pull/%s \n", r.Name, pr.ID)
 		} else {
-			fmt.Printf("   #%s not ready - %s \n", pr.ID, pr.Date)
-			parse, _ := time.Parse(pr.Date, "01-12-2020")
-			fmt.Printf("%s", parse)
+			fmt.Printf("   #%s not ready - %s \n", pr.ID, creationDateAgo)
+		}
+
+		staleDate := time.Now().AddDate(0, 0, -20)
+		if parsedDate.Before(staleDate) {
+			fmt.Printf("   #%s staled - %s \n", pr.ID, creationDateAgo)
 		}
 	}
+}
+
+// Check if repository has opened PRs
+func (r *Repository) hasOpenedPRs() bool {
+	for _, pr := range r.PRs {
+		if pr.isOpened() {
+			return true
+		}
+	}
+	return false
 }
 
 type PR struct {
@@ -57,6 +79,19 @@ func (pr *PR) isOpened() bool {
 	return false
 }
 
+func (pr *PR) isValidCI(repo string) bool {
+	ghReposCmd, _, err := gh.Exec("pr", "--repo", repo, "checks", pr.ID, "--fail-fast", "--watch")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(ghReposCmd.String())
+	str := ghReposCmd.String()
+	prs := splitOutput(str)
+	fmt.Println(prs[len(prs)-3])
+	return true
+}
+
 func splitOutput(output string) []string {
 	return strings.Split(output, "\n")
 }
@@ -65,12 +100,28 @@ func splitLine(line string) []string {
 	return strings.Split(line, "\t")
 }
 
+func timeAgo(then time.Time) string {
+	now := time.Now()
+	diff := now.Sub(then)
+
+	switch {
+	case diff < time.Hour:
+		return fmt.Sprintf("%d minutes ago", int(diff.Minutes()))
+	case diff < time.Hour*24:
+		return fmt.Sprintf("%d hours ago", int(diff.Hours()))
+	default:
+		days := int(diff.Hours() / 24)
+		return fmt.Sprintf("%d days ago", days)
+	}
+}
+
 func main() {
 	owner := "OpenSourcePolitics"
-	ghReposCmd, _, err := gh.Exec("repo", "list", owner, "--limit", "10")
+	ghReposCmd, _, err := gh.Exec("repo", "list", owner, "--limit", "80")
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	repos := splitOutput(ghReposCmd.String())
 	var repositories []Repository
 
@@ -102,6 +153,9 @@ func main() {
 	}
 
 	for _, repo := range repositories {
-		repo.resume()
+		if !repo.hasOpenedPRs() {
+			continue
+		}
+		repo.resumeInLines()
 	}
 }
